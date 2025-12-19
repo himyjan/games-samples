45ffe8cafe52877945d34502b0e9ee5c09dd426a
Rahul Kumar Sharma <47045074+RKS13D@users.noreply.github.com>
2025-12-19 12:26:47 +0530

    Project Initialization
    Initial Unreal Project with Git Ignore and Attributes for LFS

---
+++ trivialkart/trivialkart_unreal/Source/TrivialKart_Unreal/TrivialKart_Unreal.Build.cs
@@ -0,0 +1,21 @@
+using UnrealBuildTool;
+
+public class TrivialKart_Unreal : ModuleRules
+{
+	public TrivialKart_Unreal(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ trivialkart/trivialkart_unreal/Source/TrivialKart_Unreal/TrivialKart_Unreal.cpp
@@ -0,0 +1,4 @@
+#include "TrivialKart_Unreal.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TrivialKart_Unreal, "TrivialKart_Unreal" );
